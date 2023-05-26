06d0d30181a0ed48e25e6d7767128da68dddd12d
AnaBogdan <119337018+AnaBogdan@users.noreply.github.com>
2023-05-26 03:41:33 +0300

    Update stewordle.c

--- stewordle.c
+++ stewordle.c
@@ -7,9 +7,9 @@
 #define totalDrivers 52
 
 typedef struct {
-    char name[50];
-    char team[50];
-    char country[50];
+    char* name;
+    char* team;
+    char* country;
     int carNumber;
     int debutYear;
     int wins;
@@ -46,7 +46,7 @@ Driver drivers[] = {
     {"Yuki Tsunoda", "AlphaTauri", "Japan", 22, 2021, 0},
     {"Alex Albon", "Williams", "Thailand", 23, 2019, 0},
     {"Zhou Guanyu", "Alfa Romeo", "China", 24, 2022, 0},
-    {"Jean-Éric Vergne", "Torro Rosso", "France", 25, 2012, 0},
+    {"Jean-Eric Vergne", "Torro Rosso", "France", 25, 2012, 0},
     {"Daniil Kvyat", "AlphaTauri", "Russia", 26, 2014, 0},
     {"Nico Hülkenberg", "Haas", "Germany", 27, 2010, 0},
     {"Will Stevens", "Marussia", "United Kingdom", 28, 2014, 0},
@@ -58,7 +58,7 @@ Driver drivers[] = {
     {"Lewis Hamilton", "Mercedes", "United Kingdom", 44, 2007, 103},
     {"Mick Schumacher", "Haas", "Germany", 47, 2021, 0},
     {"Alexander Rossi", "Marussia", "United States", 53, 2014, 0},
-    {"Carlos Sainz Jr.", "Ferrari", "Spain", 55, 2015, 1},
+    {"Carlos Sainz", "Ferrari", "Spain", 55, 2015, 1},
     {"George Russell", "Mercedes", "United Kingdom", 63, 2019, 1},
     {"Valtteri Bottas", "Alfa Romeo", "Finland", 77, 2013, 10},
     {"Oscar Piastri", "McLaren", "Australia", 81, 2023, 0},
@@ -67,9 +67,28 @@ Driver drivers[] = {
     {"Jack Aitken", "Williams", "United Kingdom", 89, 2020, 0},
     {"Pascal Wehrlein", "Sauber", "Germany", 94, 2016, 0},
     {"Roberto Merhi", "Marussia", "Spain", 98, 2015, 0},
-    {"Antonio Giovinazzi", "Haas", "Italy", 99, 2019, 0},
+    {"Antonio Giovinazzi", "Haas", "Italy", 99, 2017, 0},
     {"Adrian Sutil", "Sauber", "Germany", 99, 2007, 0}
 };
+
+void initializeDrivers() {
+    for (int i = 0; i < totalDrivers; i++) {
+        drivers[i].name = strdup(drivers[i].name);
+        drivers[i].team = strdup(drivers[i].team);
+        drivers[i].country = strdup(drivers[i].country);
+    }
+}
+
+void cleanupDrivers() {
+for (int i = 0; i < totalDrivers; i++) {
+        free(drivers[i].name);
+        free(drivers[i].team);
+        free(drivers[i].country);
+        drivers[i].name = NULL;
+        drivers[i].team = NULL;
+        drivers[i].country = NULL;
+    }
+}
 
 
 int findDriverIndex(char* name) {
@@ -146,11 +165,10 @@ void printDriverbad(int index, int correct) {
 int main() {
 srand(time(NULL));
 echo();
-char playerName[50];
 int i, found = 0, count = 0;
-char guess[50];
+char* guess = malloc(50 * sizeof(char)); 
 int index, aux;
-
+initializeDrivers();
 // Initialize ncurses
 initscr();
 cbreak();
@@ -170,25 +188,42 @@ init_pair(2, COLOR_RED, COLOR_BLACK);        // Incorrect guess
 init_pair(3, COLOR_MAGENTA, COLOR_BLACK);    // Too high car number
 init_pair(4, COLOR_YELLOW, COLOR_BLACK);     // Too low car number
 
-printw("Welcome to the Formula 1 Driver Guessing Game!\n");
-printw("\n");
+printw("+----------------------- Welcome to the Formula 1 Driver Guessing Game! ----------------------+\n");
+printw("\nColor Explanation:\n");
+printw("Correct: ");
+attron(COLOR_PAIR(1));  // Set color pair 1 (correct guess)
+printw("Driver details displayed in this color indicate a correct detail.\n");
+attroff(COLOR_PAIR(1)); // Turn off color pair 1
 
-printw("Guess the driver!\n");
-printw("Enter the name corresponding to the driver's name:\n");
+printw("Incorrect: ");
+attron(COLOR_PAIR(2));  // Set color pair 2 (incorrect guess)
+printw("Driver details displayed in this color indicate an incorrect detail.\n");
+attroff(COLOR_PAIR(2)); // Turn off color pair 2
+printw("Number is too high: ");
+attron(COLOR_PAIR(3));  
+printw("Driver details displayed in this color to indicate that the correct number is lower.\n");
+attroff(COLOR_PAIR(3)); 
+printw("Number is too low: ");
+attron(COLOR_PAIR(4));  
+printw("Driver details displayed in this color to indicate that the correct number is higher.\n");
+attroff(COLOR_PAIR(4)); 
+
+
+printw("\n");
+printw("Guess the driver! You have only 4 tries!\n");
 i = rand() % totalDrivers;
 aux = findDriverIndex(drivers[i].name);
-printw("\nDriver Name: %s\n", drivers[i].name);
 refresh();
 
-while (!found && count < 6) {
+while (!found && count < 4) {
     printw("Enter your guess: ");
     getstr(guess);
     index = findDriverIndex(guess);
-    printw("+----------------------+-----------------+-----------------+------------+------------+--------+\n");
-    printw("| Driver               | Team            | Country         | Car Number | Debut Year | Wins   |\n");
-    printw("+----------------------+-----------------+-----------------+------------+------------+--------+\n");
 
-    if (index != -1) {
+    if (index >= 0 && index < totalDrivers) {
+        printw("+----------------------+-----------------+-----------------+------------+------------+--------+\n");
+        printw("| Driver               | Team            | Country         | Car Number | Debut Year | Wins   |\n");
+        printw("+----------------------+-----------------+-----------------+------------+------------+--------+\n");
         if (strcmp(drivers[index].name, drivers[i].name) == 0) {
             attron(COLOR_PAIR(1));  // Set color pair 1 (correct guess)
             printDriver(index);
@@ -209,14 +244,30 @@ while (!found && count < 6) {
             attroff(A_BOLD);
         }
     } else {
-        printw("Invalid guess. Please try again.\n");
+        attron(A_BOLD);
+        printw("+----------------------------- Invalid guess. Please try again! ------------------------------+\n");
+        attroff(A_BOLD);
     }
 }
+
+    if(found != 1) {
+        attron(A_BOLD);
+        printw("+-------------------------- GAME OVER! HERE IS THE CORRECT ANSWER: ---------------------------+\n");
+        attron(COLOR_PAIR(1));  // Set color pair 1 (correct guess)
+        printw("+----------------------+-----------------+-----------------+------------+------------+--------+\n");
+        printw("| Driver               | Team            | Country         | Car Number | Debut Year | Wins   |\n");
+        printw("+----------------------+-----------------+-----------------+------------+------------+--------+\n");
+        printDriver(aux);
+        printw("+----------------------+-----------------+-----------------+------------+------------+--------+\n");
+        attroff(COLOR_PAIR(1)); // Turn off color pair 1
+        attroff(A_BOLD);
+    }
 
 getch();
 // Clean up ncurses
 endwin();
-
+cleanupDrivers();
+free(guess);
 return 0;
 
 
